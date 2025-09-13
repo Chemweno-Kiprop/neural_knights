d9073c8f01bf39e8ef22d54365c50f62072d9818
chemweno-kiprop <collinskiprop59@gmail.com>
2025-09-13 23:57:12 +0300

    Added 15 seconds delay time to spawning part of script to allow the publisher to completely load before spawning

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,46 +2,60 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
-
 
 
 def generate_launch_description():
 
+    package_name = 'neural_knights'  # <--- make sure this matches your package name
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='neural_knights' #<--- CHANGE ME
-
+    # Robot State Publisher (publishes robot_description)
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory(package_name),
+                'launch',
+                'rsp.launch.py'
+            )
+        ]),
+        launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
+    # Launch Gazebo with the empty.world (has factory plugin)
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('gazebo_ros'),
+                'launch',
+                'gazebo.launch.py'
+            )
+        ]),
+        launch_arguments={
+            'world': os.path.join(
+                get_package_share_directory('gazebo_ros'),
+                'worlds',
+                'empty.world'
+            )
+        }.items()
+    )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
-                        output='screen')
+    # Spawner node
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description',
+                   '-entity', 'my_bot'],
+        output='screen'
+    )
 
+    # Delay spawner to wait for Gazebo to be ready
+    delayed_spawn = TimerAction(period=15.0, actions=[spawn_entity])
 
-
-    # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
-        spawn_entity,
+        delayed_spawn
     ])
